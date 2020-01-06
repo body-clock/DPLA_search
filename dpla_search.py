3c08834c680b72be14ab70c16f72fa33e65f11ed
Patrick Perkins <plp248@nyu.edu>
2020-01-05 23:58:59 -0500

    styled accounted for missing key

--- dpla_search.py
+++ dpla_search.py
@@ -10,7 +10,9 @@ load_dotenv()
 api_key = os.getenv("DPLA_API_KEY")
 
 # GUI
-search_layout = [[sg.Text('What would you like to search?'), sg.InputText(key='_TERM_')],
+sg.theme("DarkRed1")
+
+search_layout = [[sg.Text('What would you like to search?'), sg.InputText(key='Term', size=(20,50))],
                  [sg.Button('Search')]]
 
 main_layout = [[sg.Frame('Input', search_layout)]]
@@ -22,13 +24,15 @@ def get_data_based_on_search_term(term):
     search_url = f'https://api.dp.la/v2/items?q={term}&api_key={api_key}'
     r = requests.get(search_url)
 
-    if r.status_code == 200:
+    if r.status_code == 200 and term != '':
         # convert to json and extract "docs" list
         search_data = json.loads(r.text)
         total_data_list = search_data["docs"]
         return total_data_list
-    else:
+    elif r.status_code != 200:
         print(f"Error:{r.status_code}")
+    else:
+        print("Invalid search term.")
 
 
 # grab what we need from the total data
@@ -37,11 +41,12 @@ def extract_relevant_data(total_data_list):
         extracted_data_list = []
         for result in total_data_list:
             # add extracted data to a new dict
-            # TODO account for missing keys
-            extracted_data_dict = {}
-            extracted_data_dict["_ID_"] = result["id"]
-            extracted_data_dict["_INGEST_DATE_"] = result["ingestDate"]
-            extracted_data_dict["_DATA_PROVIDER_"] = result["dataProvider"]
+            extracted_data_dict = {"ID": result["id"], "Ingest Date": result["ingestDate"]}
+            # account for lack of provider
+            if result["dataProvider"]:
+                extracted_data_dict["Data Provider"] = result["dataProvider"]
+            else:
+                extracted_data_dict["Data Provider"] = "N/A"
             # append extracted data to total list
             extracted_data_list.append(extracted_data_dict)
         return extracted_data_list
@@ -52,12 +57,17 @@ def extract_relevant_data(total_data_list):
 def create_dataframe_and_convert_to_html_table(data_dict, term):
     df = pd.DataFrame(data_dict)
 
-    # TODO continue styling
     # table style
+    penn_red = "#990000"
+    penn_blue = "#011F5B"
     html = {
         df.style
-        .set_caption(f"Search results for: {term}")
-        .render()
+            .set_caption(f"Search results for: {term}")
+            .set_properties(**{'background-color': penn_blue,
+                               'color': 'white',
+                               'text-align': 'left'})
+            .set_table_attributes(f'border="5" bordercolor={penn_red}')
+            .render()
     }
     html_list = list(html)
     print(html)
@@ -71,8 +81,8 @@ program_running = True
 if program_running:
     event, values = enter_data_window.Read()
     if event == 'Search':
-        search_term = values["_TERM_"]
-        data = get_data_based_on_search_term(values["_TERM_"])
+        search_term = values["Term"]
+        data = get_data_based_on_search_term(values["Term"])
         if data:
             relevant_data = extract_relevant_data(data)
             create_dataframe_and_convert_to_html_table(relevant_data, search_term)
